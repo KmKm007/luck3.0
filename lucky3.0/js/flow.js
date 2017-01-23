2cd8370268ac63c67602b801671572201bc8fde8
hxm <dgdkhxm@163.com>
2017-01-23 17:31:14 +0800

    奖项顺序更改

--- lucky3.0/js/flow.js
+++ lucky3.0/js/flow.js
@@ -29,44 +29,18 @@
 
 	per = {
 		award: '三等奖：小熊电热水壶',
-		amount: 10,
+		amount: 20,
 		index: 3,
 		isSegment: false,
 		isSingle: false
 	}
 	flow.push(per);
 
+
 	per = {
-		award: '三等奖：小熊电热水壶',
-		amount: 10,
+		award: '三等奖：现金奖100元',
+		amount: 30,
 		index: 4,
-		isSegment: false,
-		isSingle: false
-	}
-	flow.push(per);
-
-	per = {
-		award: '三等奖：现金奖100元',
-		amount: 10,
-		index: 5,
-		isSegment: false,
-		isSingle: false
-	}
-	flow.push(per);
-
-	per = {
-		award: '三等奖：现金奖100元',
-		amount: 10,
-		index: 6,
-		isSegment: false,
-		isSingle: false
-	}
-	flow.push(per);
-
-	per = {
-		award: '三等奖：现金奖100元',
-		amount: 10,
-		index: 7,
 		isSegment: false,
 		isSingle: false
 	}
@@ -75,7 +49,7 @@
 	per = {
 		award: '二等奖：按摩器',
 		amount: 5,
-		index: 8,
+		index: 5,
 		isSegment: false,
 		isSingle: false
 	}
@@ -84,7 +58,7 @@
 	per = {
 		award: '二等奖：现金奖200元',
 		amount: 5,
-		index: 9,
+		index: 6,
 		isSegment: false,
 		isSingle: false
 	}
@@ -93,7 +67,7 @@
 	per = {
 		award: '二等奖：现金奖200元',
 		amount: 5,
-		index: 10,
+		index: 7,
 		isSegment: false,
 		isSingle: false
 	}
@@ -102,7 +76,7 @@
 	per = {
 		award: '一等奖：足浴盆',
 		amount: 2,
-		index: 11,
+		index: 8,
 		isSegment: false,
 		isSingle: false
 	}
@@ -111,7 +85,7 @@
 	per = {
 		award: '一等奖：现金奖500元',
 		amount: 2,
-		index: 12,
+		index: 9,
 		isSegment: false,
 		isSingle: false
 	}
@@ -120,7 +94,7 @@
 	per = {
 		award: '特等奖：无线吸尘器',
 		amount: 1,
-		index: 13,
+		index: 10,
 		isSegment: false,
 		isSingle: false
 	}
@@ -129,7 +103,7 @@
 	per = {
 		award: '特等奖：现金奖1000元',
 		amount: 1,
-		index: 14,
+		index: 11,
 		isSegment: false,
 		isSingle: false
 	}
